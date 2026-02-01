5a8028898564e30bf54bdcf3d50c76555276362f
firdoshh19 <firdoshh34@gmail.com>
2026-02-01 18:58:51 +0530

    Update pointer ass q4 .c

--- c programming/pointer/pointer ass q4 .c
+++ c programming/pointer/pointer ass q4 .c
@@ -1,5 +1,5 @@
  /*4.Take an integer variable and a pointer pointing to it. Now increment the value of variable using pointer. Print the value of integer before and after update.*/
- #include<stdio.h>
+ /*#include<stdio.h>
  int main(){
      int a;
      a=9;
@@ -8,7 +8,7 @@
      printf("%d\n",a);
      printf("%d\n",p);
      p=p+1;
-     printf("%d",p);
+     printf("%d",p);*/
 
 
 
